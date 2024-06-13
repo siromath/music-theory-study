598db229bb314358ca530eba487d4c04b0b0d477
siromath <sirosiromath@gmail.com>
2024-06-13 14:30:25 +0900

    update path 2

--- app/utils/index.tsx
+++ app/utils/index.tsx
@@ -1,5 +1,8 @@
+import nextConfig from "../../next.config.mjs";
 import Image from "next/image";
 
+const basePath = nextConfig.basePath || "";
+
 export function getStaffImage(name: string) {
-  return <Image loader={({ src }) => src} src={`/staff/staff_${name}.png`} width="600" height="600" alt="" />
+  return <Image loader={({ src }) => src} src={`${basePath}/staff/staff_${name}.png`} width="600" height="600" alt="" />
 }
