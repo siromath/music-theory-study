21c38485e72020cb1b57dc5fa68e2b3099ad0bc6
siromath <sirosiromath@gmail.com>
2024-06-13 14:16:40 +0900

    update path

--- app/utils/index.tsx
+++ app/utils/index.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
+import getConfig from "next/config";
+
+const { basePath } = getConfig().publicRuntimeConfig;
 
 export function getStaffImage(name: string) {
-  return <Image loader={({ src }) => src} src={`/staff/staff_${name}.png`} width="600" height="600" alt="" />
+  return <Image loader={({ src }) => src} src={`${basePath}/staff/staff_${name}.png`} width="600" height="600" alt="" />
 }
