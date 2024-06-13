f945cbd3b363296e16443abe5f5b8a15d12764a0
siromath <sirosiromath@gmail.com>
2024-06-13 14:20:52 +0900

    Revert "update path"
    This reverts commit 21c38485e72020cb1b57dc5fa68e2b3099ad0bc6.

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV == 'production';
-const subDirectory = '/music-theory-study';
-const path = isProd ? subDirectory : '';
-
 const nextConfig = {
   output: 'export',
-  basePath: path,
-  assetPrefix: path,
-  publicRuntimeConfig: {
-    basePath: path,
-  },
   images: {
     loader: 'custom',
   },
